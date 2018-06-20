a2b5110bf9d90b3e72ed3be84d3ec76c11dc94ca
Julien Castelain <julien.castelain@liferay.com>
2018-06-20 12:34:44 +0200

    Returns a function instead of an object to match babel's latest requirements

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 'use strict';
 
-module.exports = {
-  plugins: [
-    [
-      require('babel-plugin-incremental-dom').default,
-      {
-        components: true,
-        namespaceAttributes: true,
-        prefix: 'IncrementalDOM',
-        runtime: 'iDOMHelpers'
-      }
-    ]
-  ]
+module.exports = function() {
+	return {
+		plugins: [
+			[
+				require('babel-plugin-incremental-dom').default,
+				{
+					components: true,
+					namespaceAttributes: true,
+					prefix: 'IncrementalDOM',
+					runtime: 'iDOMHelpers'
+				}
+			]
+		]
+	};
 };
